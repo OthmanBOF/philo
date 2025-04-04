c67366d46ef28bcb713f488be8bb61b43acf3ce5
OthmanBOF <bouftoukothman@gmail.com>
2025-04-04 18:59:35 +0000

    f

--- mand/parsing.c
+++ mand/parsing.c
@@ -1,17 +1,25 @@
+#include "philo.h"
 
+static inline bool ft_isspace(char c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
 
+static void ft_check(char *str)
+{
 
+}
 
-
-
-
-
+static inline bool is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
 
 //i need to adjust the midel to suit the check of negative apearences and display an error then exit clean 
-
-
-
 int	parse_args(char *str)
 {
-    
+	const char	*nbr;
+	
+	while (ft_isspace(str))
+		str++;
 }
