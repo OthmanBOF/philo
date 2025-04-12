b82eaf6c1577f917814478ada944e8cec15a943a
Otman Bouftoug <obouftou@e2r5p6.1337.ma>
2025-04-12 12:37:00 +0100

    monitoring_init

--- mand/monitor.c
+++ mand/monitor.c
@@ -2,6 +2,7 @@
 
 void	monitor_dinner(void *data)
 {
+	int	i;
 	t_data	*table;
 
 	table = (t_data *)data;
@@ -9,7 +10,15 @@ void	monitor_dinner(void *data)
 		;
 	while (!simulation_finished(table))
 	{
-		
+		i = -1;
+		while (++i < table->philo_num)
+		{
+			if (philo_died(table->philo + i))
+			{
+				set_bools(&table->mutex_data, &table->end_simul, true);
+				write_status(DIED, )
+			}
+		}
 	}
 	return (NULL);
 }
